ffae8633c96ea2c243c54660904181cae8566fa2
joao-bettu <joaobettuvictor@gmail.com>
2025-05-01 15:06:44 -0300

    Apresenta mapa no terminal e função main

--- Python/main.py
+++ Python/main.py
@@ -1,6 +1,20 @@
-from operator import indexOf
 # Importa o mapa do labirinto
 from lab import lab_map, start, end
+
+def print_map (lab, initial_point, finish, actual_position):
+    for i in range(10):
+        for j in range(10):
+            if lab[i][j] == initial_point:
+                print("S", end="")
+            elif lab[i][j] == finish:
+                print("E", end="")
+            elif lab[i][j] == actual_position:
+                print("A", end="")
+            else:
+                print(f"{lab[i][j]}", end="")
+        print()
+    print()
+    return
 
 def calc_heuristc (position, destiny_pos):
     return abs(position[0] - destiny_pos[0]) + abs(position[1] - destiny_pos[1])
@@ -37,18 +51,43 @@ def get_paths (actual_pos, lab):
 
     return possible_paths
 
-def choose_path (path):
+def choose_path (path, last):
     heuristics = []
 
-    for i in path:
+    for i in range(len(path)):
         heuristics.append(calc_heuristc(path[i], end))
 
-    shortest = heuristics[0]
     index_shortest = 0
+    shortest = heuristics[index_shortest]
 
-    for i in heuristics:
+    for i in range(len(heuristics)):
         if heuristics[i] < shortest:
             shortest = heuristics[i]
-            index_shortest = indexOf(heuristics[i])
+            index_shortest = i
+        elif heuristics[i] == shortest:
+            if path[i] == last:
+                continue
 
     return path[index_shortest]
+
+def main ():
+    seen_paths = [start]
+    position = start
+    last_step = 0
+    next_step = 0
+
+    print_map(lab_map, start, end,position)
+
+    while position != end:
+        possible_path = get_paths(position, lab_map)
+        next_step = choose_path(possible_path, last_step)
+        last_step = position
+        position = next_step
+        seen_paths.append(position)
+        print_map(lab_map, start, end, position)
+
+    print("Labirinto Concluído!")
+    return
+
+if __name__ == "__main__":
+    main()
